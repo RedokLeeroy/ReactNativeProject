a490efb9dcb991c7406335c4df609d9970d53132
RedokLeeroy <Lys1999@ukr.net>
2022-11-17 18:49:09 +0200

    .

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,33 +1,75 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import * as Font from "expo-font";
 import {
+  ImageBackground,
   KeyboardAvoidingView,
   StyleSheet,
-  Text,
+  Keyboard,
+  TextInput,
   TouchableWithoutFeedback,
   View,
+  Platform,
+  Button,
 } from "react-native";
-
-const loadFonts = async () => {
-  await Font.loadAsync({
-    "Roboto-Regular": require("./img/fonts/Roboto/Roboto-Regular.ttf"),
-    "Roboto-Medium": require("./img/fonts/Roboto/Roboto-Medium.ttf"),
-  });
-};
+const image = require("./assets/img/Photo_BG.jpg");
+// const loadFonts = async () => {
+//   await Font.loadAsync({
+//     "Roboto-Regular": require("./img/fonts/Roboto/Roboto-Regular.ttf"),
+//     "Roboto-Medium": require("./img/fonts/Roboto/Roboto-Medium.ttf"),
+//   });
+// };
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
-  if (!isReady) {
-    return (
-      <AppLoading startAsync={loadFonts} onFinish={() => setIsReady(true)} />
-    );
-  }
+  // const [isReady, setIsReady] = useState(false);
+  // if (!isReady) {
+  //   return (
+  //     <AppLoading startAsync={loadFonts} onFinish={() => setIsReady(true)} />
+  //   );
+  // }
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const nameHandler = (text) => setName(text);
+  const passwordHandler = (text) => setPassword(text);
+  const emailHandler = (text) => setEmail(text);
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
-        <KeyboardAvoidingView
-          behavior={Platform.OS == "ios" ? "padding" : "height"}
-        ></KeyboardAvoidingView>
+        <ImageBackground
+          source={image}
+          resizeMode="cover"
+          style={{ width: 300, height: 500 }}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+          >
+            <View>
+              <TextInput
+                value={name}
+                onChangeText={nameHandler}
+                placeholder="Username"
+                style={styles.input}
+              />
+              <TextInput
+                value={password}
+                onChangeText={passwordHandler}
+                placeholder="Password"
+                secureTextEntry={true}
+                style={styles.input}
+              ></TextInput>
+              <TextInput
+                value={email}
+                onChangeText={emailHandler}
+                placeholder="email"
+                style={styles.input}
+              />
+              <Button title="Зарeєструватись"></Button>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -36,8 +78,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    backgroundColor: "#F6F6F6",
+    borderRadius: 8,
+    margin: 30,
+    padding: 16,
   },
 });
